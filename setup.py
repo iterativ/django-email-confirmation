7e6d474294fb38a916474c617b79b6e1d1073a23
maersu <me@maersu.ch>
2012-10-07 21:44:10 +0200

    inc version

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setup(
         ],
     },
     install_requires=[
-        'djpostman==0.4.10',
+        'djpostman==0.4.11',
     ],
     dependency_links = [ 
-        'http://github.com/iterativ/djpostman/tarball/master#egg=djpostman-0.4.10',
+        'http://github.com/iterativ/djpostman/tarball/master#egg=djpostman-0.4.11',
     ],
     zip_safe=False,
     classifiers=[
